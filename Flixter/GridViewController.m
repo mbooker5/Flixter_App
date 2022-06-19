28fb15a6f51d9fa534a1ce123214a11f86acc270
mbooker5 <mbooker734@gmail.com>
2022-06-19 12:36:13 -0700

    Added Collection View Layout

--- Flixter/GridViewController.m
+++ Flixter/GridViewController.m
@@ -10,16 +10,85 @@
 #import "UIImageView+AFNetworking.h"
 #import "DetailsViewController.h"
 
-@interface GridViewController ()
-
+@interface GridViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) NSArray *movies;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 @end
 
 @implementation GridViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
     // Do any additional setup after loading the view.
+    [self fetchMovies];
+    
 }
+
+- (void)fetchMovies {
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=f53f4a7976b1c4b5242a714f742c3764"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Network Error"
+                                              message:@"Please connect to the internet and press OK."
+                                              preferredStyle:UIAlertControllerStyleAlert];
+                
+               UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                   handler:^(UIAlertAction * action){[self fetchMovies];}];
+               [alert addAction:defaultAction];
+               [self presentViewController:alert animated:YES completion:nil];
+               
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               NSLog(@"%@", dataDictionary);
+               // TODO: Get the array of movies
+              self.movies = dataDictionary[@"results"];
+               // TODO: Store the movies in a property to use elsewhere
+               for (NSDictionary *movie in self.movies) {
+                   NSLog(@"%@", movie[@"title"]);
+               }
+               
+               [self.collectionView reloadData];
+               // TODO: Reload your table view data
+                
+           }
+        [self.refreshControl endRefreshing];
+       }];
+    
+    [task resume];
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section{
+    return self.movies.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    CollectionViewGridCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"CollectionViewGridCell" forIndexPath:indexPath];
+    
+    // using indexPath
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    // loading each poster image
+    NSString *baseURL = @"https://image.tmdb.org/t/p/w500";
+    NSString *urlString = movie[@"poster_path"];
+    NSString *fullURL = [baseURL stringByAppendingString:urlString];
+    NSURL *url = [[NSURL alloc] initWithString:fullURL];
+    
+    [cell.collectionImage setImageWithURL:url];
+    
+    return cell;
+}
+
+
+
 
 /*
 #pragma mark - Navigation
